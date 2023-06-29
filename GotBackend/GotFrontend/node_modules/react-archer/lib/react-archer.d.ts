05059af241060f24556e52ce63ac3b785dc9f504
Shyam Sawant <shyamsawant@Shyams-MacBook-Pro.local>
2023-06-29 23:15:34 +0530

    third commit

--- GotBackend/GotFrontend/node_modules/react-archer/lib/react-archer.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as ArcherElement } from './ArcherElement/ArcherElement';
-export { default as ArcherContainer } from './ArcherContainer/ArcherContainer';
-export { ArcherContainerRef } from './ArcherContainer/ArcherContainer.types';
--- GotBackend/GotFrontend/node_modules/react-archer/src/utils/encodeId.ts
+++
@@ -1,1 +0,0 @@
-export const encodeId = (id: string): string => encodeURI(id).replace(/%/g, '_');
